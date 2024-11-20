889814114d3d45b06e3e542b5e5ed3d260926fa2
vwh <vwhe@proton.me>
2024-11-20 20:07:02 +0300

    fix: fix empty inbox issue on api endpoint

--- src/app/api/inbox/[inboxid]/route.ts
+++ src/app/api/inbox/[inboxid]/route.ts
@@ -6,7 +6,15 @@ export async function GET(request: NextRequest) {
   const inboxId = request.nextUrl.pathname.split("/").pop() as string;
 
   const result = getInboxById(inboxId);
-  if (result.success) return NextResponse.json(result.data);
+  if (result.success) {
+    if (!result.data)
+      return NextResponse.json(
+        { error: "Inbox does not exist or has been deleted" },
+        { status: 404 }
+      );
+
+    return NextResponse.json(result.data);
+  }
 
   return NextResponse.json(
     { error: result.error.message },
